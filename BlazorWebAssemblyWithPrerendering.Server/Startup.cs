7bd016d44ab0a95070e766bb4d17efdd7fc1e826
Andrzej Włoszczyński <awloszczynski@hps.com.pl>
2019-10-08 17:43:58 +0200

    Response cache.

--- BlazorWebAssemblyWithPrerendering.Server/Startup.cs
+++ BlazorWebAssemblyWithPrerendering.Server/Startup.cs
@@ -22,6 +22,7 @@ namespace BlazorWebAssemblyWithPrerendering.Server
                     new[] { "application/octet-stream" });
             });
             services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
+            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
